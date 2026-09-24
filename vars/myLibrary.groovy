7eb975f296e892c5d291360a22ffa8bdbd370fb3
eilay2504@gmail.com <eilay2504@gmail.com>
2026-09-24 23:15:14 +0300

    Add shared library functions

--- vars/myLibrary.groovy
+++ vars/myLibrary.groovy
@@ -1,11 +1,43 @@
-def deployApp(String branch = 'main') {
-    echo "Deploying application from branch: ${branch}"
+def buildImage (String appName) {
+    echo "Building ${appName}"
+    echo "Build Number: ${env.BUILD_NUMBER}"
+
+    sh "docker build -t ${appName}:${env.BUILD_NUMBER} ."
 }
 
-def cleanup() {
-    echo "Running cleanup tasks"
+def pushDocker(String appName , String credentialsId) {
+    withCredentials([
+        usernamePassword(
+            credentialsId: credentialsId,
+            usernameVariable: 'USERNAME',
+            passwordVariable: 'PASSWORD'
+        )
+    ]) {
+        echo "Deploying with username ${env.USERNAME}"
+
+        sh 'echo "$PASSWORD" | docker login -u "$USERNAME" --password-stdin'
+        sh "docker tag ${appName}:${env.BUILD_NUMBER} ${env.USERNAME}/${appName}:${env.BUILD_NUMBER}"
+        sh "docker push ${env.USERNAME}/${appName}:${env.BUILD_NUMBER}"
+    }
 }
 
-def buildApp() {
-    echo "Building the application"
+def dockerImage(String appName ,int hostPort ,int containerPort ,String containerName) {
+    echo "Running ${appName}:${env.BUILD_NUMBER}"
+
+    sh "docker run -d --name ${containerName} -p ${hostPort}:${containerPort} ${appName}:${env.BUILD_NUMBER}"
+}
+
+def healthCheck(int hostPort, String healthPath, int waitTime , String host) {
+    sh "sleep ${waitTime}"
+    sh "curl --fail http://${host}:${hostPort}${healthPath}"
+}
+
+def test() {
+    echo 'Testing...'
+    // Test steps here
+}
+
+def deploy() {
+    echo 'Deploying...'
+    // Deploy steps here
 }
